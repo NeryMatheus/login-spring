e986a6ce6c4c47ff753621a6006996366476bdff
Matheus Nery <matheusnery345@gmail.com>
2024-06-18 20:30:23 -0400

    feat: create service token to generate and verify token

--- README.md
+++ README.md
@@ -1,7 +1,22 @@
 # login-spring 
 ## Dependências  
-### - Spring Web
-### - Spring Data JPA
-### - Spring Security
-### - DevTools
-### - Lombok
+###### - Spring Web
+###### - Spring Data JPA
+###### - Spring Security
+###### - DevTools
+###### - Lombok
+
+
+## 1º - Criando Token
+### Criar algoritmo de criptografia
+```
+JWT.create()
+Type Algorithm
+Hash HMAC256(<Passar uma secret key>)
+```
+
+## 2º - Validando Token
+### Validar algoritmo de criptografia
+```
+JWT.require
+```
---
+++ src/main/java/com/example/login_auth_api/infra/security/TokenService.java
@@ -0,0 +1,54 @@
+package com.example.login_auth_api.infra.security;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.example.login_auth_api.model.User;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+@Service
+public class TokenService {
+    // Método de Criação/Geração de Token
+
+    @Value("${api.security.token.secret}")
+    private String secret;
+
+    public String generateToken(User user) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+
+            String token = JWT.create()
+                    .withIssuer("login-auth-api")
+                    .withSubject(user.getEmail())
+                    .withExpiresAt(this.generateExpirationDate())
+                    .sign(algorithm);
+
+            return token;
+        } catch (JWTCreationException e) {
+            throw new RuntimeException("Error while authenticate");
+        }
+    }
+
+    public String validateToken(String token) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            return JWT.require(algorithm)
+                    .withIssuer("login-auth-api")
+                    .build()
+                    .verify(token)
+                    .getSubject();
+        } catch (JWTVerificationException e) {
+            return null;
+        }
+    }
+
+    private Instant generateExpirationDate() {
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.UTC);
+    }
+}
